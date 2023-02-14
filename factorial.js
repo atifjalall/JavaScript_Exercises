2c2bc78f82b5a7ea0cf07c58a2e8040d5d1cd312
Atif Jalal <atifjalal96@gmail.com>
2023-02-14 11:08:06 +0500

    commit

--- factorial.js
+++ factorial.js
@@ -1,7 +1,7 @@
 //Create a function that takes a number and returns its factorial.
 
 let factorial = (num) => {
-  return num == 0 ? 1 : num * myfunc(num - 1);
+  return num == 0 ? 1 : num * factorial(num - 1);
 };
 
 console.log(factorial(4)); //24
