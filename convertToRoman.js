68cca66325420133c791b97b95550e3b137e04da
Atif Jalal <atifjalal96@gmail.com>
2023-02-21 10:52:53 +0500

    commit

---
+++ convertToRoman.js
@@ -0,0 +1,30 @@
+//Write a program that converts a given number to its Roman numeral representation.
+
+let convertToRoman = (num) => {
+  var roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  var str = "";
+
+  for (var i of Object.keys(roman)) {
+    var q = Math.floor(num / roman[i]);
+    num -= q * roman[i];
+    str += i.repeat(q);
+  }
+
+  return str;
+};
+
+console.log(convertToRoman(3));
